7ba905d136697cdefa783ec9f580fda669faab0e
Nicholas Gill <mythagel@gmail.com>
2013-04-25 21:12:14 +1000

    wip v8 initial tests

--- src/jscam.cpp
+++ src/jscam.cpp
@@ -1,7 +1,23 @@
 #include <cstdio>
+#include <v8.h>
 //#include "Machine.h"
 
 int main()
 {
+	using namespace v8;
+
+	HandleScope handle_scope;
+
+	Persistent<Context> context = Context::New();
+	Context::Scope context_scope(context);
+
+	Handle<String> source = String::New("'Hello' + ', World!'");
+	Handle<Script> script = Script::Compile(source);
+	Handle<Value> result = script->Run();
+
+	context.Dispose();
+
+	String::AsciiValue ascii(result);
+	printf("%s\n", *ascii);
 	return 0;
 }
