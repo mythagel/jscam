52c35fea19adeffc63190f9b5e0ec15d7907ab55
Nicholas Gill <mythagel@gmail.com>
2013-04-27 22:04:34 +1000

    Document intended javascript api

--- src/jscam.cpp
+++ src/jscam.cpp
@@ -2,6 +2,73 @@
 #include <v8.h>
 #include "Machine.h"
 
+/*
+js api
+
+"machine":
+{
+	"type": "mill/lathe",
+	"tools":
+	[
+		{
+			"name": "tool_name",
+			"type": "mill/lathe",
+
+			"center_cutting": "true/false",
+			"flutes": "int",
+			"flute_length": "",
+			"cutting_length": "",
+			"mill_diameter": "",
+			"shank_diameter": "",
+			"core_diameter": "",
+			"length": ""
+		},
+		{...}
+	],
+	"spindle": "0-100,300,500-1000,3000"
+}
+
+void setup(object, string);
+setup(machine, "LinuxCNC")
+
+exact_path()
+exact_stop()
+path_blend()
+path_blend(p)
+path_blend(p, q)
+
+motion(absoloute/incremental)
+arc_motion(absoloute/incremental)
+
+units(metric/imperial)
+
+plane(XY/XZ...)
+
+feed_rate_mode(...)
+
+feed_rate(30.5);
+
+spindle_on(speed, direction)
+spindle_off()
+
+load_tool(id)
+tool_change(id)
+
+begin_block(name)
+end_block(restore)
+
+optional_pause(comment)
+
+rapid(xyzabc)
+linear(xyzabc)
+
+arc(...)
+
+plunge(...)
+
+
+
+ */
 int main()
 {
 	Machine machine{Machine::Type::Mill, "LinuxCNC"};
