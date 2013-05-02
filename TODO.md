6edb297bd4a267d912538141f49ce3695ad134af
mythagel <mythagel@gmail.com>
2013-05-02 10:33:11 +1000

    More visible TODO list

---
+++ TODO.md
@@ -0,0 +1,19 @@
+# TODO List #
+
+
+## jscam ##
+ * Refine js api (some functions can be consolidated)
+
+## cxxcam ##
+Auto feedrate / spindle speeds
+ * Complete Tool class
+	- Needs nef model loading / generation
+	- Store properties for different tool types
+ * Complete Stock class
+	- Needs model loading & generation
+	- Needs intersection tests
+	- Needs material properties
+ * Toolpath expansion & intersection tests for rapids
+ * Material removal volume for each operation
+	- Calculate volume of material that tool can remove and ensure that volume is appropriate.
+
--- src/jscam.cpp
+++ src/jscam.cpp
@@ -7,22 +7,6 @@
 
 v8::Persistent<v8::Context> create_context();
 
-/*
- * General TODO list for jscam / cxxcam
- * jscam:  Refine js api (some functions can be consolidated)
- * cxxcam: Auto feedrate / spindle speeds
- *       : Complete Tool class
- *         - Needs nef model loading / generation
- *         - Store properties for different tool types
- *       : Complete Stock class
- *         - Needs model loading & generation
- *         - Needs intersection tests
- *         - Needs material properties
- *       : Toolpath expansion & intersection tests for rapids
- *       : Material removal volume for each operation
- *         - Calculate volume of material that tool can remove and ensure
- *           that volume is appropriate.
- */
 int main(int argc, char* argv[])
 {
 	using namespace v8;
