d19ee0036e77397775960cbf76a34439ff3a4d89
mythagel <mythagel@gmail.com>
2013-05-17 16:02:34 +1000

    Test gcode in dev. Changes to jscam/cxxcam may be needed.

--- examples/pocket.js
+++ examples/pocket.js
@@ -4,13 +4,29 @@ init(mill, "LinuxCNC");
 /*
 pocket.js
 =========
+
+Lessons learnt in planning already:
+Need to be able to access properties from cxxcam, i.e. active tool diameter.
 */
-function pocket()
+function rubbish()
 {
 	begin_block("Rectangular Pocket dimensions...");
-	// TODO
-	end_block(machine.BlockRestore.restore);
+	for(var x = 0; x != 10 ; ++x)
+	{
+		rapid({z: 5});
+		rapid({x: x, y: 0});
+		if(x % 2 == 0)
+			linear({z: -1});
+		else
+			linear({z: -2});
+		linear({x: x, y: 10});
+	}
+	
+	end_block(machine.BlockRestore.RestoreAll);
 }
 
-pocket();
-
+begin_block("main");
+feed_rate(100);
+spindle_on(100);
+rubbish();
+end_block(machine.BlockRestore.RestoreAll);
