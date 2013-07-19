958c79a63b919b28b0eb1f9f7f6cad9dcc580ddb
mythagel <mythagel@gmail.com>
2013-07-19 13:05:33 +1000

    Add basic test for state modification.

---
+++ test/push_state.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var mill = {
+	type: "mill",
+	units: "metric",
+	axes: "XYZ",
+	tools:
+	[
+		{
+			name: "3mm End Mill",
+			id: 1,
+			type: "mill",
+			center_cutting: true,
+			flutes: 4,
+			flute_length: 25,
+			cutting_length: 20,
+			mill_diameter: 3,
+			shank_diameter: 6,
+			core_diameter: 3,
+			length: 60
+		}
+	],
+	spindle: [ {rpm: [100, 1000] }, {rpm: [2000, 6000]}]
+};
+
+var m = new Machine(mill);
+
+m.push_state();
+
+m.begin_block("Setup");
+	m.feed_rate = 100;
+	m.spindle_on(250);
+	m.preload_tool(1);
+	m.tool_change(1);
+	m.optional_pause("Post Setup");
+m.end_block("tool", "feedrate");
+
+m.push_state();
+
+m.rapid({x: 100});
+
+m.discard_state();
+
+m.pop_state();
